1c233abc71148cb93b35cad1c0b4c84626052ffe
Kristian Bowler <Kristian.bowler@iress.com>
2021-06-24 19:17:12 +0100

    Login changes

--- src/components/createthread/CreateThread.js
+++ src/components/createthread/CreateThread.js
@@ -21,6 +21,7 @@ export default function CreateThread() {
 
     return (
         <div className="createThread">
+        <div className="form">
             <label htmlFor="title"><b>Title</b></label>
             <input type="text" placeholder="Enter Title" id="title" name="title" required/>
             <br/>
@@ -28,7 +29,11 @@ export default function CreateThread() {
             <br/>
             <textarea name="text" cols="25" rows="5" id="content">
             </textarea>
+            </div>
+
+            <div className="Button">
             <button type="submit" onClick={createThread}>Login</button>
+            </div>
         </div>
     )
 }
