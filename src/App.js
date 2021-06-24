98c2fa18ebcbf1a95b5cc32b7f31f2d4c53fac4e
Jack Rogers <jack.rogers@iress.com>
2021-06-24 15:00:36 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './fonts/fonts.css';
-import './App.css';
 import Header from './components/header/header';
 import HomeUI from './components/homepage/home-page';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import ForumsHome from './components/forums/forums-homepage';
 
 function App() {
-    return (
-        <>
-            <Header/>
-            <HomeUI/>
-        </>
-    );
+  return (
+    <>
+
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="" component={HomeUI} />
+          <Route path="/forums" component={ForumsHome} />
+        </Switch>
+      </BrowserRouter>
+
+    </>
+  );
 }
 
 export default App;
