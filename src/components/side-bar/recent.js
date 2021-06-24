416932f667fdf096425968f0481c4b70c5c4c8bf
Kristian Bowler <Kristian.bowler@iress.com>
2021-06-24 22:12:34 +0100

    recent

--- src/components/side-bar/recent.js
+++ src/components/side-bar/recent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import RecentSection1 from './recentOptions';
 
+import './recent.css'
+
 function RecentlyViewed() {
     return (
         <>
@@ -10,9 +12,7 @@ function RecentlyViewed() {
                     <br />
                
                     <div id="sideBarTitle">
-                        <h1 id="sideTitle" >
-                            Recently Viewed
-                        </h1>
+                        <h2>Recently Viewed</h2>
                     </div>
 
                     <div class="option">
