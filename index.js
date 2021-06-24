bcafe0bf94ec86f75e0ab320506b3fe48104a0e7
Chris Dickinson <chris.dickinson@iress.com>
2021-06-24 10:33:57 +0100

    Added way to get categories

--- index.js
+++ index.js
@@ -10,13 +10,80 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'admin',
     password: 'password',
-    database: 'forums'
+    database: 'financeforums'
 });
 
 connection.connect(function (err) {
     if (err) throw err;
     console.log("Successfully connected to Database");
+    createTables();
 });
+
+/**
+ * Gets all categories return in json format
+ */
+app.get('/getCategories', async function (request, response) {
+    connection.query("SELECT * FROM categories",
+        (error, results) => {
+            if (error) console.log(error);
+            response.status(200).json(results);
+        })
+});
+
+function createTables() {
+    connection.query(
+        "CREATE TABLE IF NOT EXISTS users (" +
+        "id INT auto_increment," +
+        "email VARCHAR(255)," +
+        "first_name VARCHAR(255)," +
+        "last_name VARCHAR(255)," +
+        "password VARCHAR(255)," +
+        "avatar LONGTEXT," +
+        "role VARCHAR(255)," +
+        "primary key (id)" +
+        ");"
+    );
+    connection.query(
+        "CREATE TABLE IF NOT EXISTS threads (" +
+        "id INT auto_increment," +
+        "author INT," +
+        "title VARCHAR(255)," +
+        "date DATETIME," +
+        "content LONGTEXT," +
+        "primary key (id)" +
+        ");"
+    );
+    connection.query(
+        "CREATE TABLE IF NOT EXISTS comments (" +
+        "id INT auto_increment," +
+        "author INT," +
+        "thread INT," +
+        "date DATETIME," +
+        "content LONGTEXT," +
+        "primary key (id)" +
+        ");"
+    );
+    connection.query(
+        "CREATE TABLE IF NOT EXISTS reactions (" +
+        "id INT auto_increment," +
+        "threadID INT," +
+        "commentID INT," +
+        "userID INT," +
+        "reactionType VARCHAR(255)," +
+        "primary key (id)" +
+        ");"
+    );
+    connection.query(
+        "CREATE TABLE IF NOT EXISTS categories (" +
+        "id INT auto_increment," +
+        "category_name VARCHAR(255)," +
+        "category_icon VARCHAR(255)," +
+        "primary key (id)" +
+        ");"
+    );
+    console.log("Successfully created Tables");
+
+}
 
 app.listen(3001, function () {
     console.log("Listening on port 3001")
