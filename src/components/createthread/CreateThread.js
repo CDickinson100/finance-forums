458d9c9f5ded4b321de820be2ec1800fb0f42e80
Chris Dickinson <chris.dickinson@iress.com>
2021-06-24 20:08:24 +0100

    Added ability to select category

--- src/components/createthread/CreateThread.js
+++ src/components/createthread/CreateThread.js
@@ -2,9 +2,23 @@ import React, {useEffect, useState} from 'react';
 import './CreateThread.css';
 
 export default function CreateThread() {
+
+    const [categories, setCategories] = useState([]);
+
+    async function getCategories() {
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'}
+        };
+        const response = await fetch('/getCategories', requestOptions);
+        const body = await response.json();
+        setCategories(body);
+    }
+
     async function createThread() {
         const title = document.getElementById('title').value;
         const content = document.getElementById('content').value;
+        const category = document.getElementById('categories').value;
         const requestOptions = {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
@@ -12,27 +26,38 @@ export default function CreateThread() {
                 'token': localStorage.token,
                 'title': title,
                 'content': content,
-                'category': 1
+                'category': category
             })
         };
         const response = await fetch('/createThread', requestOptions);
         window.location = "/";
     }
 
+    useState(() => {
+        getCategories();
+    }, [])
+
     return (
         <div className="createThread">
-        <div className="form">
-            <label htmlFor="title"><b>Title</b></label>
-            <input type="text" placeholder="Enter Title" id="title" name="title" required/>
-            <br/>
-            <label htmlFor="content"><b>Content</b></label>
-            <br/>
-            <textarea name="text" cols="25" rows="5" id="content">
+            <div className="form">
+                <label htmlFor="categories">Choose a Category:</label>
+                <select name="categories" id="categories">
+                    {categories.map(value => {
+                        return <option value={value.id}>{value.category_name}</option>
+                    })}
+                </select>
+                <br/>
+                <label htmlFor="title"><b>Title</b></label>
+                <input type="text" placeholder="Enter Title" id="title" name="title" required/>
+                <br/>
+                <label htmlFor="content"><b>Content</b></label>
+                <br/>
+                <textarea name="text" cols="25" rows="5" id="content">
             </textarea>
             </div>
 
             <div className="Button">
-            <button type="submit" onClick={createThread}>Login</button>
+                <button type="submit" onClick={createThread}>Login</button>
             </div>
         </div>
     )
