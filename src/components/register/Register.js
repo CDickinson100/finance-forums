b65620f73f3c403b1c670392c55af285bc0a2417
GhostRunners25 <73123755+IsaacButler25@users.noreply.github.com>
2021-06-25 07:46:41 +0100

    register styled

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -10,7 +10,7 @@ export default function Register() {
         const password = document.getElementById('password').value;
         const requestOptions = {
             method: 'POST',
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 'email': email,
                 'firstName': firstName,
@@ -20,48 +20,30 @@ export default function Register() {
         };
         const response = await fetch('/registerUser', requestOptions);
         const body = await response.json();
-        if(body.message){
+        if (body.message) {
             document.getElementById("incorrect").innerHTML = body.message;
-        }else{
+        } else {
             window.location = "/login";
         }
     }
 
     return (
         <div className="register">
-        <br></br>
-        <div className="form">
-        <br></br>
-            <h1>Register</h1>
-            <br></br>
-            <br></br>
-            <label htmlFor="email"><b>Email</b></label>
-            <br></br>
-            <input type="text" placeholder="Enter Email" id="email" name="email" required/>
-            <br></br>
-
-            <label htmlFor="firstName"><b>First Name</b></label>
-            <br></br>
-            <input type="text" placeholder="First Name" id="firstName" name="firstName" required/>
-            <br></br>
-
-            <label htmlFor="lastName"><b>Last Name</b></label>
-            <br></br>
-            <input type="text" placeholder="Last Name" id="lastName" name="lastName" required/>
-            <br></br>
-
-            <label htmlFor="password"><b>Password</b></label>
-            <br></br>
-            <input type="password" placeholder="Enter Password" id="password" name="password" required/>
-            <br></br>
-
+            <div className="form">
+                <h1>Register</h1>
+                <input type="text" placeholder="Enter your email" id="email" name="email" required />
+                <input type="text" placeholder="Enter your first name" id="firstName" name="firstName" required />
+                <input type="text" placeholder="Enter your last name" id="lastName" name="lastName" required />
+                <input type="password" placeholder="Enter your password" id="password" name="password" required />
             </div>
 
-            <p id="incorrect"/>
+
 
             <div className="Button">
-            <button type="submit" onClick={create}>Create Account</button>
+                <button type="submit" onClick={create}>Create Account</button>
             </div>
+
+            <p id="incorrect" />
 
         </div>
     )
