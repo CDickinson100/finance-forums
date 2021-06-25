80beb30dc95a4751b36e86b9b1e48a11f1c7d970
Chris Dickinson <chris.dickinson@iress.com>
2021-06-25 09:39:01 +0100

    Added reactions

---
+++ src/components/reactions/Reactions.js
@@ -0,0 +1,54 @@
+import React, {useEffect, useState} from 'react';
+import like from '../../icons/like.png';
+import './Reactions.css';
+
+export default function Reactions(props) {
+    const {id} = props;
+
+    const [reactions, setReactions] = useState([]);
+
+    async function getReactions() {
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'id': id
+            })
+        };
+        const response = await fetch('/getThreadReactions', requestOptions);
+        const body = await response.json();
+        setReactions(body);
+    }
+
+    async function addReaction() {
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'threadID': id,
+                'token': localStorage.token,
+                'reaction': "like"
+            })
+        };
+        const response = await fetch('/addThreadReactions', requestOptions);
+        getReactions();
+    }
+
+    useEffect(() => {
+        getReactions();
+    }, [])
+
+    return localStorage.token && (
+        <div className="reaction">
+            <button onClick={() => addReaction()}>
+                <img src={like} height="30px"/>
+                {
+                    reactions.length > 0 &&
+                    <>
+                        <p>{reactions.length}</p>
+                    </>
+                }
+            </button>
+        </div>
+    )
+}
--- src/components/threads/Thread.js
+++ src/components/threads/Thread.js
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactMarkdown from 'react-markdown';
 import MenuSections from "../homepage/menu/menu-sections";
 import Comments from "../comments/Comments";
 import './Thread.css';
+import Reactions from "../reactions/Reactions";
 
 export default function Thread(props) {
-    const { id } = props.match.params;
+    const {id} = props.match.params;
 
     const [thread, setThread] = useState([]);
 
     async function getThread() {
         const requestOptions = {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
                 'id': id
             })
@@ -29,13 +30,14 @@ export default function Thread(props) {
     return thread.length === 1 && (
         <>
             <div className="menu">
-                <MenuSections defaultCollapsed={true} />
+                <MenuSections defaultCollapsed={true}/>
             </div>
             <div id="thread">
                 <h1>{thread[0].title}</h1>
                 <ReactMarkdown>{thread[0].content}</ReactMarkdown>
+                <Reactions id={id}/>
             </div>
-            <Comments id={id} />
+            <Comments id={id}/>
         </>
     )
 }
